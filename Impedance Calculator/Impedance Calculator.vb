3d4ef2d9c20ea186069a2a997eb5aae3a2a0ebc2
JoshuaMakuch <joshuamakuch9@gmail.com>
2023-02-13 09:09:37 -0700

    Included part of the series calculation

--- Impedance Calculator/Impedance Calculator.vb
+++ Impedance Calculator/Impedance Calculator.vb
@@ -1,17 +1,68 @@
 'Joshua Makuch
 'Spring 2023
 'Impedance Calculator
-'
+'https://github.com/JoshuaMakuch/Impedance-Calculator.git
+
+'Asks the user if it wants to do a parallel or series calculation, then it runs a function that then asks for how many impedances are in the network, once it has done that it
+'asks the user to input the values in rectangular (real and agineryim) then stores those valuse into two arrays the size of the number of impedances
 
 Option Explicit On
 Option Strict On
 
 Imports System
+Imports System.Reflection.Metadata.Ecma335
 
 Module ImpedanceCalcultor
     Sub Main()
 
+        Dim userInput As String
+        Dim totalImpedance() As String
 
+        Do
+
+            Console.WriteLine(vbCrLf & "Parallel or Series? S/P")
+            userInput = Console.ReadLine()
+
+            If userInput.ToLower() = "s" Then
+                totalImpedance = Series()
+                Console.Clear()
+                For i = 0 To 3
+                    Console.Write(totalImpedance(i) & " ")
+                Next
+            ElseIf userInput.ToLower() = "p" Then
+
+            End If
+
+
+        Loop
 
     End Sub
+
+    Function Series() As String()
+
+        Dim totalSeriesImpedance(4) As String
+        Dim userInput As String
+
+        Do
+            Console.WriteLine("How many impedances?")
+            userInput = Console.ReadLine
+            Try
+                Dim real(CInt(userInput)) As Double
+                Dim fake(CInt(userInput)) As Double
+                Console.WriteLine($"You chose {userInput} impedances.")
+                Exit Do
+            Catch ex As Exception
+                Console.WriteLine("That is not a whole number")
+            End Try
+        Loop
+
+
+
+        Return totalSeriesImpedance
+
+    End Function
+
 End Module
+
+
+
