ebd42acb88a6dfad2c5dea84ac0dabe02e2afcab
JoshuaMakuch <joshuamakuch9@gmail.com>
2023-02-13 08:09:36 -0700

    Add project files.

---
+++ Impedance Calculator/Impedance Calculator.vb
@@ -0,0 +1,17 @@
+'Joshua Makuch
+'Spring 2023
+'Impedance Calculator
+'
+
+Option Explicit On
+Option Strict On
+
+Imports System
+
+Module ImpedanceCalcultor
+    Sub Main()
+
+
+
+    End Sub
+End Module
